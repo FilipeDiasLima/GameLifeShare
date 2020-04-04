641f417c829ea95b8374993e7170d268ca93b3e3
Filipe Dias <filipdiasdelima@gmail.com>
2020-04-04 07:35:30 -0300

    gamerlifeshare

--- backend/src/database/migrations/20200402221614_create_users.js
+++ backend/src/database/migrations/20200402221614_create_users.js
@@ -3,6 +3,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('users', function (table) {
     table.string('id').primary();
     table.string('name').notNullable();
+    table.string('username').notNullable();
     table.string('email').notNullable();
     table.string('password').notNullable();
   });
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,12 +3,14 @@ const { celebrate, Segments, Joi } = require('celebrate');
 
 const UsersControllers = require('./controllers/UsersControllers');
 
+
 const routes = express.Router();
 
 //criar usuario
 routes.post('/users', celebrate({
   [Segments.BODY]: Joi.object().keys({
     name: Joi.string().required(),
+    username: Joi.string().required(),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   })
@@ -16,7 +18,11 @@ routes.post('/users', celebrate({
 
 //listar todos os usuarios
 routes.get('/users', UsersControllers.index);
+
 //listar um unico usuario
 routes.get('/users/:id', UsersControllers.indexInd);
+
+//deletar usuarios
+routes.delete('/users/:id', UsersControllers.delete);
 
 module.exports = routes;
