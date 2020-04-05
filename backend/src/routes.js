70b4aae2209cd0913b6332256dd566c876b993db
Abraao Rodrigues <abraao503@gmail.com>
2020-04-05 11:57:04 -0400

    routes atualizado

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,26 +1,13 @@
 const express = require('express');
 
 const UsersControllers = require('./controllers/UsersControllers');
+const LoginControllers = require('./controllers/LoginControllers');
 
 const registerValidation = require('./validators/register');
 const emailAlreadyExists = require('./utils/emailAlreadyExists');
 const hashPassword = require('./utils/hashPassword');
-const checkPassword = require('./utils/checkPassword');
-
-const LoginControllers = require('./controllers/LoginControllers');
-
 
 const routes = express.Router();
-
-//login
-routes.post('/login',celebrate({
-  [Segments.BODY]: Joi.object().keys({
-    username: Joi.string().required(),
-    password: Joi.string().required(),
-  })
-}), LoginControllers.create);
-
-//criar usuario
 
 routes.post('/users', async (req, res) => {
     const {error} = registerValidation(req.body);
@@ -36,27 +23,7 @@ routes.post('/users', async (req, res) => {
     UsersControllers.create(req, res);
 });
 
-routes.post('/login', async (req, res) =>{
-  const dataUser = await emailAlreadyExists(req.body);
-
-  if (!dataUser) return res.status(400).send('Email is wrong');
-
-  const validPass = await checkPassword(req.body.password, dataUser.password);
-
-  if(!validPass) return res.status(400).send('password is wrong');
-
-  res.send('logged in!');
-});
-
-routes.post('/users', celebrate({
-  [Segments.BODY]: Joi.object().keys({
-    name: Joi.string().required(),
-    username: Joi.string().required(),
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  })
-}), UsersControllers.create);
-
+routes.post('/login', LoginControllers.create);
 
 //listar todos os usuarios
 routes.get('/users', UsersControllers.index);
