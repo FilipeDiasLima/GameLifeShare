831a1a9f886d0d2f9b25175cbab056028bab0fcb
Abraao Rodrigues <abraao503@gmail.com>
2020-04-08 17:40:51 -0400

    .

--- backend/src/controllers/FilesControllers.js
+++ backend/src/controllers/FilesControllers.js
@@ -3,14 +3,12 @@ const connection = require('../database/connection');
 module.exports = {
     async create(request, response){
         const { originalname, size, filename, path } = request.file;
-        const postId =  3;
 
         const [id] = await connection('files').insert({
             name: originalname,
             key: filename,
             size,
             url: path,
-            postId
         })
 
         response.json({ id, originalname, path });
