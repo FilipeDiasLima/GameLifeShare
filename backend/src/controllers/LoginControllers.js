94e52b9faf765a03fa73cdc22853e8911d0ab6ad
Abraao Rodrigues <abraao503@gmail.com>
2020-04-07 20:50:00 -0400

    adicionado retorno o de token

--- backend/src/controllers/LoginControllers.js
+++ backend/src/controllers/LoginControllers.js
@@ -1,16 +1,33 @@
 const userAlreadyExists = require('../utils/userAlreadyExists');
 const checkPassword = require('../utils/checkPassword');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 module.exports = {
   async create(request, response){
     const dataUser = await userAlreadyExists(request.body);
 
-    if (!dataUser) return response.status(400).send('Username is wrong');
+    if (!dataUser) return response.status(400).send('Password or email is wrong');
 
     const validPass = await checkPassword(request.body.password, dataUser.password);
 
-    if(!validPass) return response.status(400).send('password is wrong');
+    if(!validPass) return response.status(400).send('Password or email is wrong');
 
-    return response.json(dataUser);
+    const token = jwt.sign( {id: dataUser.id}, process.env.TOKEN_SECRET,{
+      expiresIn: '7d'
+    });
+
+    const {name, username, email} = dataUser;
+
+    return response.json({
+      user: {
+        name,
+        username,
+        email
+      },
+      token
+    });
   }
 }
