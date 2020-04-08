2cf833904f72d396a08f3f6f02d7124cbd3dec82
Abraao Rodrigues <abraao503@gmail.com>
2020-04-08 17:47:14 -0400

    adicionado novas rotas de postagem e comentarios

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const multer = require('multer');
 
+const CommentsControllers = require('./controllers/CommentsControllers');
 const UsersControllers = require('./controllers/UsersControllers');
 const LoginControllers = require('./controllers/LoginControllers');
 const PostsControllers = require('./controllers/PostsControllers');
 const FilesControllers = require('./controllers/FilesControllers');
 
 const multerConfig = require('./config/multer');
-
 const hashPassword = require('./utils/hashPassword');
-
 const auth = require('./middlewares/auth');
+
 const checkDataRegister = require('./middlewares/checkDataRegister');
+const commentEditAuth = require('./middlewares/commentEditAuth');
+const postEditAuth = require('./middlewares/postEditAuth');
 
 const routes = express.Router();
 
@@ -33,8 +35,18 @@ routes.get('/users/:id', UsersControllers.indexInd);
 //deletar usuarios
 routes.delete('/users/:id', UsersControllers.delete);
 
-routes.use(auth);
+routes.get('/posts', PostsControllers.index);
+
+routes.get('/comments/:id', CommentsControllers.indexId);
+
+routes.use(auth); //apenas usuarios logados podem acessar as rotas abaixo
 
 routes.post('/posts', PostsControllers.create);
+
+routes.delete('/posts/:id', postEditAuth, PostsControllers.delete);
+
+routes.post('/comments', CommentsControllers.create);
+
+routes.delete('/comments/:id', commentEditAuth, CommentsControllers.delete);
 
 module.exports = routes;
