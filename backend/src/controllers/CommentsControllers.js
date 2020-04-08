f498ee2e19b88860a1362b8e6d132d72ac47f85a
Abraao Rodrigues <abraao503@gmail.com>
2020-04-08 17:38:56 -0400

    adicionado metodos de delete e indexId

--- backend/src/controllers/CommentsControllers.js
+++ backend/src/controllers/CommentsControllers.js
@@ -1,12 +1,22 @@
 const connection = require('../database/connection');
 
 module.exports = {
+    async indexId(request, require){
+        const { id } = request.params;
+
+        const posts = await connection('comments')
+            .where('postId',id)
+            .select('*')
+
+        return response.json(posts)
+    },
+
     async create(request, response){
         const { body, postId } = request.body;
         const { userId } = request;
         const createdAt = new Date().toISOString();
 
-        const [id] = await connection('posts').insert({
+        const [id] = await connection('comments').insert({
             body,
             createdAt,
             postId,
@@ -14,5 +24,15 @@ module.exports = {
         });
 
         response.json({ id });
+    },
+
+    async delete(request, response){
+        const { id } = request.params;
+
+        await connection('comments')
+          .where('id', id)
+          .delete();
+    
+        return response.status(204).send();
     }
 }
